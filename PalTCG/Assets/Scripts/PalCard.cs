e689fc448eca041a5d6e4685473c998014911dea
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-06-13 22:12:31 -0400

    and a bit more

--- PalTCG/Assets/Scripts/PalCard.cs
+++ PalTCG/Assets/Scripts/PalCard.cs
@@ -1,8 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class PalCard : CardData
 {
-    
+    public UnityEvent<GameObject> OnDestroy;
+    public UnityEvent<GameObject> OnPlay;
+    public UnityEvent<GameObject> ActivateAbility;
 }
