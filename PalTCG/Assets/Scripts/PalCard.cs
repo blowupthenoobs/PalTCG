3e34c26967cb7e785c52c8862afa392c0c95de9e
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-06-15 20:30:52 -0400

    Set Up Resource Management

--- PalTCG/Assets/Scripts/PalCard.cs
+++ PalTCG/Assets/Scripts/PalCard.cs
@@ -6,6 +6,8 @@ using UnityEngine.Events;
 [CreateAssetMenu(menuName = "Card/PalCard")]
 public class PalCard : CardData
 {
+    [SerializeField] Resources.Element element;
+    [SerializeField] Resources.Traits traits;
     public UnityEvent<GameObject> OnDestroy;
     public UnityEvent<GameObject> OnPlay;
     public UnityEvent<GameObject> ActivateAbility;
