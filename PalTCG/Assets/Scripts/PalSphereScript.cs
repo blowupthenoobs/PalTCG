8ea493142155f72180c47b732b78b81027b7ef50
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-07-07 17:48:09 -0400

    Made a new Prefab

--- PalTCG/Assets/Scripts/PalSphereScript.cs
+++ PalTCG/Assets/Scripts/PalSphereScript.cs
@@ -4,15 +4,7 @@ using UnityEngine;
 
 public class PalSphereScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] GameObject cardPrefab;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    public GameObject heldCard;
 }
