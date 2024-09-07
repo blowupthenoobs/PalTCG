31755f7f77b7d1e8dcc3e61e5336850d7b8bfa52
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-09-07 15:24:40 -0400

    Removed Remnants from PlayerPalSphere code

--- PalTCG/Assets/Scripts/AdversaryScripts/EnemyPalSphereScript.cs
+++ PalTCG/Assets/Scripts/AdversaryScripts/EnemyPalSphereScript.cs
@@ -19,26 +19,15 @@ public class EnemyPalSphereScript : MonoBehaviour
 
     public void SelectAsTarget()
     {
-        Debug.Log(HandScript.Instance.state);
-        if(GameManager.Instance.phase == "targeting" && heldCard != null)
+        if(HandScript.Instance.state == "targeting" && heldCard != null)
         {
-            if(HandScript.Instance.selected != null && heldCard == null && HandScript.Instance.state == "default")
-            {
-                if(HandScript.Instance.selected.GetComponent<CardScript>() != null)
-                {
-                    GameManager.Instance.ShowConfirmationButtons();
-                    HandScript.Instance.state = "raiding";
-                    Debug.Log(HandScript.Instance.state);
-                    // HandScript.Instance.updateSelection += VerifyButtons;
-                    // ConfirmationButtons.Instance.Confirmed += PayForCard;
-                    // ConfirmationButtons.Instance.Denied += Disengage;
-                    // ConfirmationButtons.Instance.Denied += HandScript.Instance.ClearSelection;
-                }
-            }
-            else if(HandScript.Instance.state == "lookingForSphere")
-            {
-                HandScript.Instance.Select(gameObject);
-            }
+            GameManager.Instance.ShowConfirmationButtons();
+            HandScript.Instance.state = "raiding";
+            Debug.Log("now running");
+            // HandScript.Instance.updateSelection += VerifyButtons;
+            // ConfirmationButtons.Instance.Confirmed += PayForCard;
+            // ConfirmationButtons.Instance.Denied += Disengage;
+            // ConfirmationButtons.Instance.Denied += HandScript.Instance.ClearSelection;
         }
     }
 
