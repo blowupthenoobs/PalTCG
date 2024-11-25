478ccce782756e35704a2d56db3eaa8d5a6b1ddf
blowupthenoobs <88066026+blowupthenoobs@users.noreply.github.com>
2024-11-24 22:01:05 -0500

    Set Up Deck Builder UI

---
+++ PalTCG/Assets/Scripts/UIManagement/ScalingGridScript.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScalingGridScript : MonoBehaviour
+{
+    [SerializeField] Vector2 defaultGridDimensions;
+    [SerializeField] Vector2 defaultSpacingDimensions;
+    [SerializeField] Vector2 maxGridDimensions;
+    private GridLayoutGroup grid;
+    [SerializeField] bool isVerticle;
+    void Start()
+    {
+        grid = GetComponent<GridLayoutGroup>();
+
+        UpdateCellSizes();
+    }
+
+    void UpdateCellSizes()
+    {
+        Vector2 gridSize = GetComponent<RectTransform>().rect.size;
+
+        Vector2 newGridDimensions = new Vector2();
+        Vector2 newSpacingDimensions = new Vector2();
+        
+        if(!isVerticle)
+        {
+            float percentToMake = gridSize.x / (defaultGridDimensions.x * maxGridDimensions.x + defaultSpacingDimensions.x * (maxGridDimensions.x - 1));
+            newGridDimensions = defaultGridDimensions * percentToMake;
+            newSpacingDimensions = defaultSpacingDimensions * percentToMake;
+        }
+
+
+        grid.cellSize = newGridDimensions;
+        grid.spacing = newSpacingDimensions;
+    }
+}
